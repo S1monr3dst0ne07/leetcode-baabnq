78857bbc99b4a0963882f8b58b5a5149f67ff43b
S1mon <jaforsi.kozak@gmail.com>
2024-10-04 17:31:03 +0200

    refactored formatting of prototype of problem 9

--- 9. Palindrome Number/proto.c
+++ 9. Palindrome Number/proto.c
@@ -1,14 +1,15 @@
+#include <stdbool.h>
+
 #define SIGN_BIT ((unsigned)1 << 31)
 #define INT_LIMIT (SIGN_BIT-1)
 
 int reverse(int in)
 {
     int out = 0;
-    while (in)
+    for (; in; in /= 10)
     {
         if (out > (INT_LIMIT / 10)) return 0;
-        out = out * 10 + (in % 10);
-        in /= 10;
+        out = out*10 + (in%10);
     }
 
     return out;
